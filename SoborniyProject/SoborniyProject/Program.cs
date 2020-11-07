d03e7850daff5868defd45c0880081648ce56db4
Nevoliashka <41319097+Nevoliashka@users.noreply.github.com>
2020-11-07 21:30:04 +0200

    feat(settings): add load env

---
+++ SoborniyProject/SoborniyProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoborniyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,28 @@
+﻿using DotNetEnv;
+using System;
+using System.IO;
+
+
+namespace SoborniyProject
+{
+    
+    public readonly struct Settings
+    {    
+        public static void Load()
+        {
+            Env.Load(envFile);
+            MYSQL_HOST = Env.GetString("MYSQL_HOST", "localhost");
+            MYSQL_PORT = Env.GetInt("MYSQL_PORT", 3306);
+            MYSQL_USER = Env.GetString("MYSQL_USER", "root");
+            MYSQL_PASSWORD = Env.GetString("MYSQL_PASSWORD", "1234");
+            MYSQL_DATABASE = Env.GetString("MYSQL_DATABASE", "soborniy");
+        }
+
+        public static string MYSQL_HOST { get; set; }
+        public static int MYSQL_PORT { get; set; }
+        public static string MYSQL_USER { get; set; }
+        public static string MYSQL_PASSWORD { get; set; }
+        public static string MYSQL_DATABASE { get; set; }
+        public static string envFile = "../../.env";
+    }
+}
