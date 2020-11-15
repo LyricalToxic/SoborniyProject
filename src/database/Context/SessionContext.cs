434c44ecc1335fa78e27ebf35a061f819b52df99
Nevoliashka <41319097+Nevoliashka@users.noreply.github.com>
2020-11-15 17:50:15 +0200

    feat(db): add session model

---
+++ src/database/Context/SessionContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoborniyProject.database.Models;
+
+namespace SoborniyProject.database.Context
+{
+    public class SessionContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Connection.GetString());
+        }
+        public SessionContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Session>().Property(c => c.Key).IsRequired().HasMaxLength(32);
+            modelBuilder.Entity<Session>().Property(c => c.TotalTime).HasDefaultValue("0");
+            modelBuilder.Entity<Session>().Property(c => c.CreatedAt)
+                .IsRowVersion();
+            modelBuilder.Entity<Session>().Property(c => c.UpdatedAt)
+                .ValueGeneratedOnAddOrUpdate();
+        }
+        public DbSet<Session> Session { get; set; }
+    }
+}
---
+++ src/database/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace SoborniyProject.database.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public int Status { get; set; }
+        public string TotalTime { get; set; }
+        public DateTime CreatedAt{ get; set; } 
+        public DateTime UpdatedAt{ get; set; } 
+    }
+}
